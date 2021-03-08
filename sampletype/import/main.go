5b7c029134c03537ea53f4c41bb3bd81738463cd
blck-snwmn <whitesnowmancreator@gmail.com>
2021-03-09 03:51:14 +0900

    Add sample

--- sampletype/import/main.go
+++ sampletype/import/main.go
@@ -13,7 +13,18 @@ const bar = `package main
 
 import "fmt"
 
+type (
+	xxxx struct {
+		int
+		string
+	}
+	yyyy struct {
+		xi int
+		xs string
+	}
+)
 
+func (xxxx)Test(){}
 
 const template = "Hello, world.\nHello %s.\n"
 
@@ -26,10 +37,15 @@ func greet(name string) {
 	fmt.Printf(template, name)
 }
 
-
 func main() {
-	greet("sara")
-	greet("")
+	var name1 = "sara"
+	const name2 = "tom"
+	greet(name1)
+	innerFunc := func(name string){
+		greet(name)
+	}
+	innerFunc(name2)
+	innerFunc("")
 }`
 
 func main() {
@@ -50,11 +66,55 @@ func main() {
 	fmt.Println()
 
 	scope := pkg.Scope()
-	fmt.Printf("scope's names = %v\n", scope.Names())
-	fmt.Printf("lookup `main` = %v\n", scope.Lookup("main"))
-	fmt.Printf("lookup `defaultName` = %v\n", scope.Lookup("defaultName"))
-	fmt.Printf("lookup `greet` = %v\n", scope.Lookup("greet"))
-	fmt.Printf("lookup `template` = %v\n", scope.Lookup("template"))
+	names := scope.Names()
+	fmt.Printf("scope's names = %v\n", names)
+	for _, n := range names {
+		obj := scope.Lookup(n)
+		fmt.Printf("lookup `%s` = %v[%v]\n", n, obj, obj.Type())
+	}
 	fmt.Printf("lookup `mainx` = %v\n", scope.Lookup("mainx"))
+	fmt.Println()
 
+	mainFunc, _ := scope.Lookup("main").(*types.Func)
+	fmt.Printf("mainFunc: String() = %v\n", mainFunc.String())
+	fmt.Printf("mainFunc: FullName() = %v\n", mainFunc.FullName())
+	fmt.Printf("mainFunc: Type() = %v\n", mainFunc.Type())
+	fmt.Printf("mainFunc: Scope() = %v\n", mainFunc.Scope())
+	fmt.Println()
+
+	greetFunc, _ := scope.Lookup("greet").(*types.Func)
+	fmt.Printf("greetFunc: Type() = %v\n", greetFunc.Type())
+	fmt.Println()
+
+	templateConst, _ := scope.Lookup("template").(*types.Const)
+	fmt.Printf("templateConst: String() = %v\n", templateConst.String())
+	fmt.Printf("templateConst: Type() = %v\n", templateConst.Type())
+	fmt.Printf("templateConst: Val() = %v\n", templateConst.Val())
+	fmt.Println()
+
+	xxxxTypeName, _ := scope.Lookup("xxxx").(*types.TypeName)
+	fmt.Printf("xxxxStruct: Type() = %v\n", xxxxTypeName.Type())
+	fmt.Printf("xxxxStruct: Parent() = %v\n", xxxxTypeName.Parent())
+	fmt.Printf("xxxxStruct: Exported() = %v\n", xxxxTypeName.Exported())
+	fmt.Println()
+
+	xxxxNames, _ := xxxxTypeName.Type().(*types.Named)
+	fmt.Printf("xxxxNames: NumMethods() = %v\n", xxxxNames.NumMethods())
+	fmt.Printf("xxxxNames: Underlying() = %v\n", xxxxNames.Underlying())
+	fmt.Printf("xxxxNames: Obj() = %v\n", xxxxNames.Obj())
+	fmt.Println()
+
+	s, _ := xxxxNames.Underlying().(*types.Struct)
+	fmt.Printf("xxxxNames: NumFields() = %v\n", s.NumFields())
+	fmt.Printf("xxxxNames: Field(0) = %v (Anonymous? %t)\n", s.Field(0), s.Field(0).Anonymous())
+	fmt.Printf("xxxxNames: Field(1) = %v (Anonymous? %t)\n", s.Field(1), s.Field(1).Anonymous())
+	fmt.Println()
+
+	yyyyTypeName, _ := scope.Lookup("yyyy").(*types.TypeName)
+	yyyyNames, _ := yyyyTypeName.Type().(*types.Named)
+	syyyy, _ := yyyyNames.Underlying().(*types.Struct)
+	fmt.Printf("xxxxNames: NumFields() = %v\n", syyyy.NumFields())
+	fmt.Printf("xxxxNames: Field(0) = %v (Anonymous? %t)\n", syyyy.Field(0), syyyy.Field(0).Anonymous())
+	fmt.Printf("xxxxNames: Field(1) = %v (Anonymous? %t)\n", syyyy.Field(1), syyyy.Field(1).Anonymous())
+	fmt.Println()
 }
