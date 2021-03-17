abf6d9806da111a81ed96910df1fbe4f54334e56
blck-snwmn <whitesnowmancreator@gmail.com>
2021-03-18 00:54:27 +0900

    Add sample

--- implsamplers/src/main.rs
+++ implsamplers/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 trait Bar {
     fn zzzzz(&self) -> String;
 }
@@ -8,10 +10,29 @@ impl<T> Bar for T {
     }
 }
 
+trait BazSelf {
+    fn to_self(&self) -> Self;
+}
+
+impl<T: Display> BazSelf for &T {
+    fn to_self(&self) -> Self {
+        self
+    }
+}
 struct Bob;
+
+impl Display for Bob {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "hello, bob!")
+    }
+}
 
 fn main() {
     println!("Hello, world!");
-    let bob = Bob {};
-    println!("{}", bob.zzzzz())
+    let bob = &Bob {};
+    println!("{}", bob.zzzzz());
+    let b = bob.to_self();
+    println!("{}", bob);
+    println!("{}", bob.to_self());
+    println!("{}", b);
 }
