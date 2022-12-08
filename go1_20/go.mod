5953fdabb73546e5049c54be6eb2168720305bcd
blck-snwmn <whitesnowmancreator@gmail.com>
2022-12-09 03:39:00 +0900

    Add go1.20rc1 sample

--- go1_20/go.mod
+++ go1_20/go.mod
@@ -1,3 +1,10 @@
 module github.com/blck-snwmn/playground-go/go1_20
 
 go 1.19
+
+require golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
+
+require (
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+)
--- go1_20/main.go
+++ go1_20/main.go
@@ -1,12 +1,62 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"strings"
 	"time"
+
+	"go.uber.org/multierr"
+	"golang.org/x/xerrors"
 )
 
 func main() {
+	// context
+	{
+		ctx := context.Background()
+		ctx, cancel := context.WithCancelCause(ctx)
+		e := errors.New("context error")
+		go func() {
+			cancel(e)
+		}()
+	tset:
+		for {
+			select {
+			case <-ctx.Done():
+				break tset
+			}
+		}
+		err := context.Cause(ctx)
+		fmt.Println(err)
+		fmt.Println(err == e) // true
+	}
+	// err
+	{
+		err1 := xerrors.New("error1")
+		err2 := xerrors.New("error2")
+		err3 := xerrors.New("error3")
+		errj := errors.Join(err1, err2, err3)
+
+		fmt.Println(errj)
+		fmt.Printf("%+v\n", errj) // no stacktrace. join call error's Error method
+		fmt.Println(errors.Is(errj, err1))
+		fmt.Println(errors.Is(errj, err2))
+		fmt.Println(errors.Is(errj, err3))
+
+		errc := multierr.Combine(err1, err2, err3) // with stacktrace
+		fmt.Printf("%+v\n", errc)
+	}
+	// fmt
+	{
+		err1 := errors.New("first_error")
+		err2 := errors.New("second_error")
+		merr := fmt.Errorf("first err is %w, second err is %w", err1, err2)
+		fmt.Println(merr)
+		fmt.Println(errors.Is(merr, err1))
+		fmt.Println(errors.Is(merr, err2))
+	}
+
 	// strings
 	fmt.Println(strings.TrimPrefix("aaabbbcccc", "aa"))
 	fmt.Println(strings.CutPrefix("aaabbbcccc", "aa"))
