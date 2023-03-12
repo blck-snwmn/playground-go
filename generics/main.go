91c00ed8311ceb047c81c874b900f54fa70390fa
blck-snwmn <whitesnowmancreator@gmail.com>
2023-03-13 01:03:58 +0900

    Change sample

--- generics/main.go
+++ generics/main.go
@@ -2,25 +2,41 @@ package main
 
 import "fmt"
 
-type AA[T any] interface {
-	New() T
+type curve[Point point[Point]] struct {
+	newPoint func() Point
 }
 
-type BB[Aa AA[Aa]] struct {
-	elm Aa
+type point[T any] interface {
+	Do(T, []byte) (T, error)
 }
 
-func (b BB[Aa]) Dd() Aa {
-	return b.elm.New()
+func NewFoo() *Foo {
+	return &Foo{id: "1"}
 }
 
-type C struct{}
+type Foo struct {
+	id string
+}
 
-func (c C) New() C {
-	return C{}
+func (f *Foo) Do(ff *Foo, b []byte) (*Foo, error) {
+	return &Foo{id: ff.id + string(b)}, nil
+}
+
+var fooCurve = curve[*Foo]{
+	newPoint: NewFoo,
 }
 
 func main() {
-	bc := BB[C]{}
-	fmt.Printf("%#v\n", bc.Dd().New())
+	f := fooCurve.newPoint()
+
+	print := func(f *Foo) {
+		fmt.Printf("%v\n", f)
+	}
+
+	f, _ = f.Do(f, []byte("aaaaa"))
+	print(f)
+	f, _ = f.Do(f, []byte("bbbbb"))
+	print(f)
+	f, _ = f.Do(f, []byte("ccccc"))
+	print(f)
 }
