2a9b4cd466196cbd0f57c48e1bfc1fb5a21cff78
blck-snwmn <whitesnowmancreator@gmail.com>
2023-07-26 02:24:09 +0900

    Add range over functions sample

--- rangefunc/main.go
+++ rangefunc/main.go
@@ -8,4 +8,26 @@ func main() {
 	for i := range 5 {
 		fmt.Println(i)
 	}
+	fmt.Println("----")
+	for i := range f {
+		if i < 40 {
+			continue
+		}
+		if i == 50 {
+			fmt.Println("break in for loop")
+			break
+		}
+		fmt.Println(i)
+	}
+}
+
+func f(yield func(int64) bool) bool {
+	for i := range 100 {
+		i := int64(i)
+		if !yield(i) {
+			fmt.Println("break in f")
+			return false // break
+		}
+	}
+	return false
 }
