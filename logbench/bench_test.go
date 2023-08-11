a79c64ac09e8362e802d00a612a140ff9e7cefef
blck-snwmn <whitesnowmancreator@gmail.com>
2023-08-12 03:05:50 +0900

    Add slog bench

---
+++ logbench/bench_test.go
@@ -0,0 +1,91 @@
+package logbench
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"testing"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func Benchmark_slog(b *testing.B) {
+	uid := uuid.NewString()
+	l := slog.New(slog.NewJSONHandler(io.Discard, &slog.HandlerOptions{AddSource: true}))
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l := l.With(slog.String("user_id", uid))
+		l.Info("this is a test. this is error message",
+			slog.String("code", "aaaaaaaa"),
+			slog.String("message", "this is error message"),
+		)
+	}
+}
+
+func Benchmark_slog_format(b *testing.B) {
+	uid := uuid.NewString()
+	l := slog.New(slog.NewJSONHandler(io.Discard, &slog.HandlerOptions{AddSource: true}))
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l := l.With(slog.String("user_id", uid))
+		l.Info("this is a test. this is error message",
+			slog.String("code", fmt.Sprintf("aa%saa", "aaaa")),
+			slog.String("message", "this is error message"),
+		)
+	}
+}
+
+func Benchmark_zap(b *testing.B) {
+	uid := uuid.NewString()
+	cfg := zap.NewProductionConfig()
+	l, _ := zap.NewProduction(
+		zap.ErrorOutput(zapcore.AddSync(io.Discard)),
+		zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+			return zapcore.NewCore(
+				zapcore.NewJSONEncoder(cfg.EncoderConfig),
+				zapcore.AddSync(io.Discard),
+				cfg.Level,
+			)
+		}))
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l := l.With(zap.String("user_id", uid))
+		l.Info("this is a test. this is error message",
+			zap.String("code", "aaaaaaaa"),
+			zap.String("message", "this is error message"),
+		)
+		l.Sync()
+	}
+}
+
+func Benchmark_zap_sugar(b *testing.B) {
+	uid := uuid.NewString()
+	cfg := zap.NewProductionConfig()
+	ll, _ := zap.NewProduction(
+		zap.ErrorOutput(zapcore.AddSync(io.Discard)),
+		zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+			return zapcore.NewCore(
+				zapcore.NewJSONEncoder(cfg.EncoderConfig),
+				zapcore.AddSync(io.Discard),
+				cfg.Level,
+			)
+		}))
+	l := ll.Sugar()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		l := l.With(zap.String("user_id", uid))
+		l.Info("this is a test. this is error message",
+			zap.String("code", "aaaaaaaa"),
+			zap.String("message", "this is error message"),
+		)
+		ll.Sync()
+	}
+}
---
+++ logbench/go.mod
@@ -0,0 +1,10 @@
+module github.com/blck-snwmn/playground-go/logbench
+
+go 1.21.0
+
+require (
+	github.com/google/uuid v1.3.0
+	go.uber.org/zap v1.25.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
