0503952e8444b19afbf5426264b1eca58667f3e1
blck-snwmn <whitesnowmancreator@gmail.com>
2023-12-16 22:23:54 +0900

    Fix comment

--- gotime/main.go
+++ gotime/main.go
@@ -19,18 +19,18 @@ func main() {
 	{
 		fmt.Println("=====start summer time=====")
 		d := time.Date(2023, 3, 12, 1, 59, 59, 0, la)
-		p(d) // 2023-03-12 01:59:59 -0500 EST
+		p(d) // 2023-03-12 01:59:59 -0800 PST
 
 		d = d.Add(time.Second)
-		p(d) // 2023-03-12 03:00:00 -0400 EDT
+		p(d) // 2023-03-12 03:00:00 -0700 PDT
 	}
 	{
 		fmt.Println("=====end summer time=====")
 		d := time.Date(2023, 11, 5, 1, 59, 59, 0, la)
-		p(d) // 2023-11-05 01:59:59 -0400 EDT
+		p(d) // 2023-11-05 01:59:59 -0700 PDT
 
 		d = d.Add(time.Second)
-		p(d) // 2023-11-05 01:00:00 -0500 EST
+		p(d) // 2023-11-05 01:00:00 -0800 PST
 	}
 	{
 		fmt.Println("==========")
