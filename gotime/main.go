6d1d0fd38a224417551926a23cf771b278fe3d75
blck-snwmn <whitesnowmancreator@gmail.com>
2023-12-16 22:11:06 +0900

    Change location

--- gotime/main.go
+++ gotime/main.go
@@ -6,16 +6,16 @@ import (
 )
 
 func main() {
+	location, _ := time.LoadLocation("America/Los_Angeles")
+	la := location
+
 	p := func(t time.Time) {
 		fmt.Printf("%v, %v, %v\n", t, t.UTC(), t.Unix())
 	}
 
-	location, _ := time.LoadLocation("America/New_York")
-	newYork := location
-
 	{
 		fmt.Println("=====start summer time=====")
-		d := time.Date(2023, 3, 12, 1, 59, 59, 0, newYork)
+		d := time.Date(2023, 3, 12, 1, 59, 59, 0, la)
 		p(d) // 2023-03-12 01:59:59 -0500 EST
 
 		d = d.Add(time.Second)
@@ -23,7 +23,7 @@ func main() {
 	}
 	{
 		fmt.Println("=====end summer time=====")
-		d := time.Date(2023, 11, 5, 1, 59, 59, 0, newYork)
+		d := time.Date(2023, 11, 5, 1, 59, 59, 0, la)
 		p(d) // 2023-11-05 01:59:59 -0400 EDT
 
 		d = d.Add(time.Second)
