6ddca6f8c3069b7f9c0520a1f5d6651f0d5c52a1
blck-snwmn <whitesnowmancreator@gmail.com>
2024-01-16 22:30:20 +0900

    Add genmod.sh script for initializing Go modules

---
+++ genmod.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 引数が与えられていない場合はエラーメッセージを表示して終了
+if [ "$#" -ne 1 ]; then
+    echo "使用法: $0 <ディレクトリ名>"
+    exit 1
+fi
+
+# 引数で指定されたディレクトリ名を変数に代入
+DIR_NAME=$1
+
+# ディレクトリを作成
+mkdir -p "$DIR_NAME"
+
+# 作成したディレクトリに移動
+cd "$DIR_NAME"
+
+# 現在のディレクトリパスを取得し、'github.com'までのパスを抽出
+CURRENT_PATH=$(pwd)
+GITHUB_PATH=$(echo $CURRENT_PATH | sed -n 's/.*\(github\.com.*\)/\1/p')
+
+# go mod init コマンドを実行
+go mod init $GITHUB_PATH/$DIR_NAME
