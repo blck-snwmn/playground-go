c44015b1bc8f9ce65b31672e8ecd4ac80a103bd3
blck-snwmn <whitesnowmancreator@gmail.com>
2024-01-28 21:12:53 +0900

    Add dockertest

---
+++ dockertest/db/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+  id   VARCHAR(255)  NOT NULL PRIMARY KEY,
+  name text  NOT NULL,
+  bio  text
+);
--- dockertest/main_test.go
+++ dockertest/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -8,13 +9,13 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-txdb"
+	"github.com/blck-snwmn/playground-go/dockertest/dockertest/db"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/ory/dockertest/v3"
 )
 
-var db *sql.DB
-
 func TestMain(m *testing.M) {
+
 	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
 	pool, err := dockertest.NewPool("")
 	if err != nil {
@@ -33,16 +34,21 @@ func TestMain(m *testing.M) {
 		log.Fatalf("Could not start resource: %s", err)
 	}
 	txdb.Register("txdb", "mysql", fmt.Sprintf("root:secret@(localhost:%s)/mysql", resource.GetPort("3306/tcp")))
-	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+
+	var mdb *sql.DB
 	if err := pool.Retry(func() error {
 		var err error
-		db, err = sql.Open("mysql", fmt.Sprintf("root:secret@(localhost:%s)/mysql", resource.GetPort("3306/tcp")))
+		mdb, err = sql.Open("mysql", fmt.Sprintf("root:secret@(localhost:%s)/mysql", resource.GetPort("3306/tcp")))
 		if err != nil {
 			return err
 		}
-		return db.Ping()
+		return mdb.Ping()
 	}); err != nil {
 		log.Fatalf("Could not connect to database: %s", err)
+	}
+	_, err = mdb.Exec(db.Schema)
+	if err != nil {
+		log.Fatalf("Could not create table: %s", err)
 	}
 
 	code := m.Run()
@@ -55,8 +61,39 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func TestSomething(t *testing.T) {
-	_, err := sql.Open("txdb", "xxx")
+func TestCreate1(t *testing.T) {
+	t.Parallel()
+	txdb, err := sql.Open("txdb", "xxx")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer txdb.Close()
+
+	q := db.New(txdb)
+	_, err = q.CreateUser(context.Background(), db.CreateUserParams{
+		ID:   "411c34d8-0110-4b2a-85d6-104603562b83",
+		Name: "xxx_name_1",
+		Bio:  sql.NullString{String: "xxx1", Valid: true},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreate2(t *testing.T) {
+	t.Parallel()
+	txdb, err := sql.Open("txdb", "yyy") // not xxx
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer txdb.Close()
+
+	q := db.New(txdb)
+	_, err = q.CreateUser(context.Background(), db.CreateUserParams{
+		ID:   "411c34d8-0110-4b2a-85d6-104603562b83",
+		Name: "xxx_name_2",
+		Bio:  sql.NullString{String: "xxx2", Valid: true},
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
