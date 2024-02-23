78253302662d404dbe1ca88773acd8cedad81a67
blck-snwmn <whitesnowmancreator@gmail.com>
2024-02-23 22:31:26 +0900

    Show deleted elements

--- slice/main.go
+++ slice/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
@@ -22,4 +24,12 @@ func main() {
 	fmt.Printf("data: %X\n", data)
 	fmt.Printf("data2: %X\n", data2)
 	fmt.Printf("data3: %X\n", data3)
+	{
+		s := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+		oldlen := len(s)
+		fmt.Println(s)
+		s = append(s[:2], s[4:]...)
+		fmt.Println(s)
+		fmt.Println(s[len(s):oldlen]) // show deleted elements
+	}
 }
