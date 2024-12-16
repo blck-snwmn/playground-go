2b6a6a6fd7694826618431b848438f57745e9a6c
blck-snwmn <whitesnowmancreator@gmail.com>
2024-12-17 01:30:53 +0900

    Add go1.24rc1

---
+++ go1_24/go.mod
@@ -0,0 +1,3 @@
+module github.com/blck-snwmn/playground-go/go1_24/go1_24
+
+go 1.24rc1
---
+++ go1_24/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// strings
+	for s := range strings.Lines("abc\ndef\nghi\n") {
+		fmt.Print(s)
+	}
+	for s := range strings.SplitSeq("abc,def,ghi", ",") {
+		fmt.Println(s)
+	}
+	for s := range strings.SplitAfterSeq("abc,def,ghi", ",") {
+		fmt.Println(s)
+	}
+	for i, s := range strings.Fields("abc\ndef\tghi jkl") {
+		fmt.Printf("%d: %s\n", i, s)
+	}
+	for i, s := range strings.FieldsFunc("ddddaxxxxxbyyyy", func(r rune) bool {
+		return r == 'a' || r == 'b'
+	}) {
+		fmt.Printf("%d: %s\n", i, s)
+	}
+	// bytes
+	for bs := range bytes.Lines([]byte("abc\ndef\nghi\n")) {
+		fmt.Print(string(bs))
+	}
+	for bs := range bytes.SplitSeq([]byte("abc,def,ghi"), []byte(",")) {
+		fmt.Println(string(bs))
+	}
+	for bs := range bytes.SplitAfterSeq([]byte("abc,def,ghi"), []byte(",")) {
+		fmt.Println(string(bs))
+	}
+	for i, bs := range bytes.Fields([]byte("abc\ndef\tghi jkl")) {
+		fmt.Printf("%d: %s\n", i, string(bs))
+	}
+	for i, bs := range bytes.FieldsFunc([]byte("ddddaxxxxxbyyyy"), func(r rune) bool {
+		return r == 'a' || r == 'b'
+	}) {
+		fmt.Printf("%d: %s\n", i, string(bs))
+	}
+}
