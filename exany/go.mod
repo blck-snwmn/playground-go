937da4d6dae716cb941c996f749ee829c7e99c13
blck-snwmn <whitesnowmancreator@gmail.com>
2025-01-03 00:47:25 +0900

    Add sample

---
+++ exany/go.mod
@@ -0,0 +1,3 @@
+module github.com/blck-snwmn/playground-go/exany/exany
+
+go 1.24rc1
---
+++ exany/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type foo struct{}
+
+func main() {
+	f := foo{}
+	do1(f)
+	do1(&f)
+	do1p(f)
+	do1p(&f)
+
+	// do1: main.foo
+	// do2: main.foo
+	// do1: *main.foo
+	// do2: *main.foo
+	// do1p: main.foo
+	// do2: *interface {}
+	// do1p: *main.foo
+	// do2: *interface {}
+
+	fmt.Println("----")
+	showReflectW(f)
+	fmt.Println("----")
+	showReflectW(&f)
+	fmt.Println("----")
+	showReflectWP(f)
+}
+
+func do1(a any) {
+	fmt.Printf("do1: %T\n", a)
+	do2(a)
+}
+
+func do1p(a any) {
+	fmt.Printf("do1p: %T\n", a)
+	do2(&a)
+}
+
+func do2(a any) {
+	fmt.Printf("do2: %T\n", a)
+}
+
+func showReflectW(a any) {
+	showReflect(a)
+}
+
+func showReflectWP(a any) {
+	showReflect(&a)
+}
+func showReflect(a any) {
+	typ := reflect.TypeOf(a)
+	fmt.Printf("Type: %v\n", typ)
+	// if type is pointer, extract type
+	if typ.Kind() == reflect.Ptr {
+		fmt.Printf("type: %v in pointer\n", typ)
+		typ = typ.Elem()
+	}
+	fmt.Printf("type: %v\n", typ)
+	// if type is interface, extract type
+	if typ.Kind() == reflect.Interface {
+		fmt.Printf("type: %v in interface1\n", typ)
+		val := reflect.ValueOf(a).Elem()
+		if val.Kind() == reflect.Interface {
+			fmt.Printf("type: %v in interface2\n", typ)
+			typ = val.Elem().Type()
+		}
+	}
+	fmt.Printf("Final Type: %v\n", typ)
+}
